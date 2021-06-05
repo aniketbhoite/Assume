4f44bcb4be7f5c669c8aac4cfa7ecaa2c1f8945f
aniketbhoite <aniketbhoite1203@gmail.com>
2021-06-05 23:17:18 +0530

    add assume generated response check test

--- processor/src/test/kotlin/com/aniketbhoite/assume/AssumeProcessorTest.kt
+++ processor/src/test/kotlin/com/aniketbhoite/assume/AssumeProcessorTest.kt
@@ -5,8 +5,12 @@ import com.google.common.truth.Truth.assertThat
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
 import org.junit.Test
+import kotlin.reflect.KTypeProjection
 import kotlin.reflect.full.companionObject
+import kotlin.reflect.full.companionObjectInstance
+import kotlin.reflect.full.createType
 import kotlin.reflect.full.functions
+import kotlin.reflect.full.starProjectedType
 
 class AssumeProcessorTest {
 
@@ -81,7 +85,7 @@ class AssumeProcessorTest {
     }
 
     @Test
-    fun `Assume function created test`() {
+    fun `Assume function created with correct name & returnType test`() {
         val result = KotlinCompilation().apply {
             sources = listOf(
                 SourceFile.kotlin(
@@ -99,7 +103,8 @@ class AssumeProcessorTest {
                              @GET("posts")
                             suspend fun getPostById(): String
                 }
-            """)
+            """
+                )
             )
 
             annotationProcessors = listOf(AssumeProcessor())
@@ -120,6 +125,68 @@ class AssumeProcessorTest {
         assertThat(kClazzCompanionObject?.functions?.size == 0).isFalse()
         val kFunc = kClazzCompanionObject?.functions?.find { it.name == "getposts" }
         assertThat(kFunc).isNotNull()
+
+        val nonNullStringType = String::class.starProjectedType
+        val nonNullIntType = Int::class.starProjectedType
+
+        val stringProjection = KTypeProjection.invariant(nonNullStringType)
+        val intProjection = KTypeProjection.invariant(nonNullIntType)
+
+        val pairClass = Pair::class
+
+        val pairReturnType = pairClass.createType(listOf(stringProjection, intProjection))
+
+        assertThat(kFunc?.returnType == pairReturnType).isTrue()
     }
 
+    @Test
+    fun `Assume generated response check test`() {
+        val result = KotlinCompilation().apply {
+            sources = listOf(
+                SourceFile.kotlin(
+                    "ApiService.kt",
+                    """
+                        import com.aniketbhoite.assume.annotations.Assume
+                        import retrofit2.http.GET
+
+                        interface NewsApiService {
+                            @Assume(
+                                responseCode = 200,
+                                response =
+                                "{\"userId\": 1, \"id\": 1, \"title\": \"Test title 1\", \"body\": \"Test title 2\"}"
+                            )
+                             @GET("posts")
+                            suspend fun getPostById(): String
+                }
+            """
+                )
+            )
+
+            annotationProcessors = listOf(AssumeProcessor())
+            inheritClassPath = true
+            messageOutputStream = System.out
+        }.compile()
+
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+        assertThat(result.messages).contains("Assume annotation processor was called")
+
+        val kClazz =
+            result.classLoader.loadClass("com.aniketbhoite.assume.mocker.AssumeClass").kotlin
+        assertThat(kClazz)
+
+        val kClazzCompanionObject = kClazz.companionObject
+        assertThat(kClazzCompanionObject)
+
+        assertThat(kClazzCompanionObject?.functions?.size == 0).isFalse()
+        val kFunc = kClazzCompanionObject?.functions?.find { it.name == "getposts" }
+        assertThat(kFunc).isNotNull()
+
+        val responsePair = kFunc?.call(kClazz.companionObjectInstance) as Pair<*, *>
+
+        assertThat(responsePair.first is String).isTrue()
+        assertThat(responsePair.first == "{\"userId\": 1, \"id\": 1, \"title\": \"Test title 1\", \"body\": \"Test title 2\"}").isTrue()
+
+        assertThat(responsePair.second is Int).isTrue()
+        assertThat(responsePair.second == 200).isTrue()
+    }
 }
